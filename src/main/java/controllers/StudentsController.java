9ef81c2427f89a062727c2925719802a22add69a
OlegKoc <koralll6@yandex.ru>
2023-03-02 21:35:35 +0400

    added class DDManager

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -17,10 +17,10 @@ public class StudentsController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-              // осединение с БД
+
         List<Student> students = DBManager.getAllActiveStudents();
         req.setAttribute("students", students); // связываем с клиентской частью
-        req.getRequestDispatcher("WEB-INF/jsp/students.jsp").forward(req, resp);   //jdbs:mysql://localhost:3306/nwe_students?user=root&password=vkznm2000
+        req.getRequestDispatcher("WEB-INF/jsp/students.jsp").forward(req, resp);
 
 
 
--- src/main/java/db/DBManager.java
+++ src/main/java/db/DBManager.java
@@ -3,7 +3,10 @@ package db;
 import entity.Group;
 import entity.Student;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,18 +23,21 @@ public class DBManager {
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); //com.mysql.cj.jdbc.Driver
-            Connection connection = DriverManager.getConnection("jdbs:mysql://localhost:3306/nwe_students?user=root&password=vkznm2000"); // получаем объект соединения с БД
+            Connection connection = DriverManager.getConnection("jdbs:mysql://localhost:3306/nwe_students?user=root&password=vkznm2000"); // соединение с   БД
             statement = connection.createStatement(); // объект для отправки запросов в БД
         } catch (Exception e) {
+            e.printStackTrace();
 
         }
     }
 
+
     public static List<Student> getAllActiveStudents() {
-        List<Student>  students =new ArrayList<>();
+        List<Student> students = new ArrayList<>();
 
         try {
-            ResultSet result = statement.executeQuery("select s.id, surname, name, date, g.group from student as s left join groupp as g on s.id_group = g.id where status = '1';"); // заносим в переменную result  ответ от БД
+            ResultSet result = statement.executeQuery("SELECT s.id, surname,  name, date, g.group from student as s left join groupp as g on s.id_group = g.id where status = '1'");
+
 
             while (result.next()) {
                 Student student = new Student();
@@ -39,18 +45,22 @@ public class DBManager {
                 student.setSurname(result.getString(SURNAME));
                 student.setName(result.getString(NAME));
                 student.setDate(result.getDate(DATE));
-                student.setID(result.getInt(ID));
+
 
                 Group group = new Group();
                 group.setName(result.getString(GROUP));
                 student.setGroup(group);
+                students.add(student);
 
             }
-        } catch (Exception e) {
+
+        } catch(Exception e) {
             e.printStackTrace();
         }
+
+
         return students;
-
-
     }
 }
+
+
